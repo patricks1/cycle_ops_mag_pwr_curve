4dbf31a87ef81147e9617f670d9532d6b4a0bb1e
Patrick Staudt <patrickstaudt1@gmail.com>
2020-06-15 20:00:51 -0400

    made separate km/hr and mi/hr versions of the result files.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-This code fits a third-order polynomial to CycleOps' Mag power curve. I got the power curve from [https://cycleops.com/post/blog-15-cycleops-science-resistance-curves](https://cycleops.com/post/blog-15-cycleops-science-resistance-curves).
+This code fits a third-order polynomial to CycleOps' Mag power curve. I got the power curve from [https://cycleops.com/post/blog-15-cycleops-science-resistance-curves](https://cycleops.com/post/blog-15-cycleops-science-resistance-curves). PowerCurve Sensor data comes from [http://www.powercurvesensor.com/cycling-trainer-power-curves](http://www.powercurvesensor.com/cycling-trainer-power-curves)
 
-See `result.png` for the resulting parameters and plot.  
-See `results.txt` for a text version of the fitted parameters to higher decimal precision.
+See `result_kph.png` and `result_mph.png` for the resulting parameters and plot.  
+See `result_kph.txt` and `result_mph.txt` for a text version of the fitted parameters to higher decimal precision.
--- powercurve.py
+++ powercurve.py
@@ -50,35 +50,44 @@ params.add('b', vary=True, value=1.)
 params.add('c', vary=True, value=1.)
 params.add('d', vary=True, value=0.)
 
-fit = minimize(resids_f, params, args=(vs_dat, pwrs_dat))
-with open('result.txt','w') as f:
-    f.write('{}'.format(fit.params))
-print(fit.params)
+def fit(vs_dat, vs_mate, label):
+    fit = minimize(resids_f, params, args=(vs_dat, pwrs_dat))
+    with open('result_{}.txt'.format(label),'w') as f:
+        f.write('{}'.format(fit.params))
+    print(fit.params)
 
-fig=plt.figure(figsize=(8,6))
-ax=fig.add_subplot(111)
-ax.plot(vs_dat, pwrs_dat, 'ob', label='data')
-ax.plot(vs_mate, pwrs_mate, 'og', label='PowerCurve Sensor data')
-ax.plot(vs_dat, pwr_f(vs_dat, fit.params), '-r', label='fit')
-if vs_dat.unit == 'mi/h' and vs_mate.unit == 'mi/h':
-    ax.set_xlabel('velocity / [mi / hr]')
-elif vs_dat.unit == 'km/h' and vs_mate.unit == 'km/h':
-    ax.set_xlabel('velocity / [km / hr]')
-else:
-    raise ValueError('unexpected velocity units')
-ax.set_ylabel('power / W')
-ax.legend()
-bbox_props=dict(boxstyle='square', fc='white') #Define props of txt box
-ax.text(-0.2,300,
-        '$P=a\cdot v^3+b\cdot v^2+c\cdot v+d$'
-        '\n$a={0:0.4f}\pm{1:0.4f}$'
-        '\n$b={2:0.2f}\pm{3:0.2f}$'
-        '\n$c={4:0.1f}\pm{5:0.1f}$'
-        '\n$d={6:0.0f}\pm{7:0.0f}$'\
-        .format(fit.params['a'].value, fit.params['a'].stderr,
-                fit.params['b'].value, fit.params['b'].stderr,
-                fit.params['c'].value, fit.params['c'].stderr,
-                fit.params['d'].value, fit.params['d'].stderr),
-        fontsize=14, bbox=bbox_props, va='top')
-plt.savefig('result.png', dpi=300)
-plt.show()
+    fig=plt.figure(figsize=(8,6))
+    ax=fig.add_subplot(111)
+    ax.plot(vs_dat, pwrs_dat, 'ob', label='data')
+    ax.plot(vs_mate, pwrs_mate, 'og', label='PowerCurve Sensor data')
+    ax.plot(vs_dat, pwr_f(vs_dat, fit.params), '-r', label='fit')
+    if vs_dat.unit == 'mi/h' and vs_mate.unit == 'mi/h':
+        ax.set_xlabel('velocity / [mi / hr]')
+    elif vs_dat.unit == 'km/h' and vs_mate.unit == 'km/h':
+        ax.set_xlabel('velocity / [km / hr]')
+    else:
+        raise ValueError('unexpected velocity units')
+    ax.set_ylabel('power / W')
+    ax.legend()
+    bbox_props=dict(boxstyle='square', fc='white') #Define props of txt box
+    ax.text(-0.2,300,
+            '$P=a\cdot v^3+b\cdot v^2+c\cdot v+d$'
+            '\n$a={0:0.4f}\pm{1:0.4f}$'
+            '\n$b={2:0.2f}\pm{3:0.2f}$'
+            '\n$c={4:0.1f}\pm{5:0.1f}$'
+            '\n$d={6:0.0f}\pm{7:0.0f}$'\
+            .format(fit.params['a'].value, fit.params['a'].stderr,
+                    fit.params['b'].value, fit.params['b'].stderr,
+                    fit.params['c'].value, fit.params['c'].stderr,
+                    fit.params['d'].value, fit.params['d'].stderr),
+            fontsize=14, bbox=bbox_props, va='top')
+    plt.savefig('result_{}.png'.format(label), dpi=300)
+    plt.show()
+    return None
+
+fit(vs_dat.to(u.imperial.mi/u.hr),
+    vs_mate.to(u.imperial.mi/u.hr),
+    'mph')
+fit(vs_dat.to(u.km/u.hr),
+    vs_mate.to(u.km/u.hr),
+    'kph')
